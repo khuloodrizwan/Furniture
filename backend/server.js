4f3e2b2958d687a07fb3f1def4d8d6f8b29d6054
khuloodrizwan <chivilkarkhulood@gmail.com>
2026-04-09 14:34:41 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,6 @@ import couponRouter from "./routes/couponRoute.js"
 // app config
 const app = express()
 const port = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${port}`);
-});
 
 // middlewares
 app.use(express.json())
