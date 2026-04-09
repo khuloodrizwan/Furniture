3d74a32808b2061e886201e0545d26b39e0a955b
khuloodrizwan <chivilkarkhulood@gmail.com>
2026-04-09 15:51:36 +0530

    done

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "import.meta.env.VITE_API_URL"
+    const url = import.meta.env.VITE_API_URL
     const [fur_list, setFurList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
